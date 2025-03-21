599ee5cab49e213b2c2e023846c6b16689724554
Divyasoni2906 <127198052+Divyasoni2906@users.noreply.github.com>
2025-03-21 23:43:15 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,11 +47,9 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+       I am a B.Tech student specializing in Electronics and Communication, I am passionate about leveraging
+my technical expertise in web development, programming, and communication systems. I aim to contribute my skills
+to a forward-thinking team while continuing to grow in dynamic environments.
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
